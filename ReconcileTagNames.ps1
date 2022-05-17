e32d0a89c20a8590b268f607c20746816697a25b
Anthony Watherston <anwather@microsoft.com>
2022-05-17 13:41:16 +1000

    Added help

--- ReconcileTagNames.ps1
+++ ReconcileTagNames.ps1
@@ -1,8 +1,55 @@
+<#
+.SYNOPSIS
+    Alter tag names and scopes while retaining values
+.DESCRIPTION
+    Modify tag names at a scope while retaining the original values. This can help when tags have incorrect casing or spelling and a uniform scheme is required. 
+.NOTES
+    Requires the Az.ResourceGraph module to be installed. 
+.LINK
+    https://github.com/anwather/ReconcileTagNames/blob/main/README.md
+.EXAMPLE
+    PS>.\ReconcileTagNames.ps1 -SubscriptionId "b01dbd36-a874-467a-99d4-0ee2cecaa474" -TagFixes @{"Cost Center"="CostCentre";"Cost Centre"="CostCentre";"CostCentre"="CostCentre"}
+
+    Displays changes to be made to tags at the subscription scope. The hashtable for the TagFixes parameter contains each incorrect spelling 
+    as the key and the corrected value as the value. Ensure that there is a single entry with the same key and value which will correct tags with incorrect casing.
+.EXAMPLE
+    PS>.\ReconcileTagNames.ps1 -SubscriptionId "b01dbd36-a874-467a-99d4-0ee2cecaa474" -TagFixes @{"Cost Center"="CostCentre";"Cost Centre"="CostCentre";"CostCentre"="CostCentre"} -UpdateTags
+
+    Updates tags at the subscription scope. The hashtable for the TagFixes parameter contains each incorrect spelling 
+    as the key and the corrected value as the value. Ensure that there is a single entry with the same key and value which will correct tags with incorrect casing.
+.EXAMPLE
+    PS>.\ReconcileTagNames.ps1 -ManagementGroupId "caf-bd" -TagFixes @{"Cost Center"="CostCentre";"Cost Centre"="CostCentre";"CostCentre"="CostCentre"} -UpdateTags
+
+    Updates tags at the management group scope. The hashtable for the TagFixes parameter contains each incorrect spelling 
+    as the key and the corrected value as the value. Ensure that there is a single entry with the same key and value which will correct tags with incorrect casing.
+.PARAMETER TagFixes
+    A hashtable of keys which represent the incorrect tag names. The value for each key should be the correct tag value. There should be a key with the same name/value used to 
+    correct incorrect casing. 
+
+    E.G. 
+
+    $tagFixes = @{
+        "businesscost" = "costcentre"
+        "Cost Center" = "costcentre"
+        "costCenntre" = "costcentre"
+        "CostCenter"  = "costcentre"
+        "costcentre"  = "costcentre"
+        "application" = "appname"
+        "Application Name" = "appname"
+        "appname" = "appname"
+    }
+
+    The script will look through the tags returned from resources - if a tag is found which matches one of the keys the tag will be replaced. Note the costcentre = costcentre value
+    in there to correct incorrect casing on tags. 
+.PARAMETER UpdateTags
+    Adding this switch parameter in will update the tags. Omitting it will simply show the changes to be made. 
+#>
+
 Param (
     [switch]$UpdateTags,
-    [Parameter(ParameterSetName = "Subscription")]
+    [Parameter(ParameterSetName = "Subscription", Mandatory = $true)]
     [string]$SubscriptionId,
-    [Parameter(ParameterSetName = "ManagementGroup")]
+    [Parameter(ParameterSetName = "ManagementGroup", Mandatory = $true)]
     [string]$ManagementGroupId,
     [hashtable]$TagFixes
 )
